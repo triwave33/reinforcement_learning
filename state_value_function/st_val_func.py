587901cd4ab487d1562d4b665a1a1034d488a32d
triwave33 <triwave33@gmail.com>
2018-07-18 22:53:40 +0900

    bug fix

--- state_value_function/st_val_func.py
+++ state_value_function/st_val_func.py
@@ -45,7 +45,15 @@ class Agent():
     def move(self, action):
         # 辞書を参照し、action名から移動量move_coordを取得
         move_coord = Agent.act_dict[action] 
-        pos_new = self.get_pos() + move_coord
+
+        # A地点
+        if (self.get_pos() == np.array([0,1])).all():
+            pos_new = [4,1]
+        # B地点
+        elif (self.get_pos() == np.array([0,3])).all():
+            pos_new = [2,3]
+        else:
+            pos_new = self.get_pos() + move_coord
         # グリッドの外には出られない
         pos_new[0] = np.clip(pos_new[0], 0, 4)
         pos_new[1] = np.clip(pos_new[1], 0, 4)
@@ -56,14 +64,12 @@ class Agent():
         # A地点
         if (state == np.array([0,1])).all():
             r = 10
-            self.set_pos([4,1])
             return r
         # B地点
         if (state == np.array([0,3])).all():
             r = 5
-            self.set_pos([2,3])
             return r
-
+    
         # グリッドの境界にいて時に壁を抜けようとした時には罰則
         if (state[0] == 0 and action == 'up'):
             r = -1
@@ -77,42 +83,49 @@ class Agent():
         else:
             r = 0
         return r
-
+    
     def pi(self, state, action):
         # 変数としてstateを持っているが、実際にはstateには依存しない
         return Agent.pi_dict1[action]
-
+    
     #def transition(self, state, action, next_state)
- 
+    
     def V_pi(self, state, n, out, iter_num):
+        #print('\nnow entering V_pi at n=%d, state=%s' %(n, str(state)))
         # state:関数呼び出し時の状態
         # n:再帰関数の呼び出し回数。関数実行時は1を指定
         # out:返り値用の変数。関数実行時は0を指定
-   
+    
         if n==iter_num:    # 終端状態
             for i, action in enumerate(Agent.actions):
                 out += self.pi(state, action) * self.reward(state,action)
+                #print("terminal condition")
             return out
         else:
             for i, action in enumerate(ACTIONS):
+                #print("action: %s" % action)
                 out += self.pi(state, action)  * agent.reward(agent.get_pos(),action) # 報酬
+                #print("before move, pos:%s" % str(agent.get_pos()))
                 self.move(action) # 移動してself.get_pos()の値が更新
-
+                #print("after move, pos:%s" % str(agent.get_pos()))
+    
                 ## 価値関数を再帰呼び出し
                 # state変数には動いた先の位置、つまりself.get_pos()を使用
                 out +=  self.pi(self.get_pos(), action) * \
                         self.V_pi(self.get_pos(), n+1, 0,  iter_num) * GAMMA
                 agent.set_pos(state) #  再帰先から戻ったらエージェントを元の地点に初期化
+                #print("agent pos set to %s, at n:%d" % (str(state), n))
             return out
 
-       
+   
 # agentの生成
-agent = Agent([0,0])
+agent = Agent([0,1])
 
-ITER_NUM = 8 # 状態価値関数推定のためにt->ITER_NUMまで辿る
+ITER_NUM = 4 # 状態価値関数推定のためにt->ITER_NUMまで辿る
 GAMMA = 0.9
 ACTIONS = ['right', 'up', 'left', 'down']
 
+agent.V_pi([0,1], 1, 0, ITER_NUM)
 
 print("状態価値関数（Vpi")
 print("%dステップ先までを計算" % ITER_NUM)
@@ -128,7 +141,7 @@ print("Vpi")
 print(v_array)
 
 
-    
+
 
 
 
