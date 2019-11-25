adbeb42006c0fb98ff1d518a4f920f1cfd7e63df
triwave33 <triwave33@gmail.com>
2019-11-25 20:28:44 +0900

    define abstract class

--- qiita/RL_11_linear_approx.py
+++ qiita/RL_11_linear_approx.py
@@ -34,9 +34,25 @@ NUM_STATE = 2
 NUM_ACTION = 3
 
 
+class BasicAgent:
+    def __init__(self,eps_ini,eps_last):
+        self.eps_ini = eps_ini
+        self.eps_last = eps_last
+
+    def getQ(self,s,a):
+        raise NotImplementedError("You have to implement getQ method.")
+
+    def select_action(self, s, eps):
+        raise NotImplementedError("You have to implement select_ation method.")
+ 
+    def update(self,s,a,reward,s_dash,a_dash):
+        raise NotImplementedError("You have to implement update method.")
+
+
 # テーブル法用のエージェントクラス
-class TableAgent:
+class TableAgent(BasicAgent):
     def __init__(self,N,alpha_ini,alpha_last,eps_ini,eps_last):    
+        super().__init__(eps_ini,eps_last)
         self.N = N # テーブルの分割数
         self.done = False 
         # 価値関数の初期化
@@ -44,10 +60,8 @@ class TableAgent:
         self.reward_array = [] 
         self.min_list = np.array([-1.2, -0.07])  # グリッド分割下限値
         self.max_list = np.array([0.6,0.07])  # グリッド分割上限値
-        self.alpha_ini = alpha_ini 
+        self.alpha_ini = alpha_ini
         self.alpha_last = alpha_last
-        self.eps_ini = eps_ini
-        self.eps_last = eps_last
  
     def digitize(self,obs): # envからの観測値をを離散化
         s = [int(np.digitize(obs[i], np.linspace(self.min_list[i], \
@@ -60,7 +74,7 @@ class TableAgent:
     def select_action(self, s, eps):
         s = self.digitize(s)
         # e-greedyによる行動選択   
-        if np.random.rand < eps: # random
+        if np.random.rand() < eps: # random
             action = np.random.randint(NUM_ACTION)
             return action
         else:
@@ -72,14 +86,14 @@ class TableAgent:
             return action
     
     # 価値関数の更新
-    def train(self,s,a,reward,s_dash,a_dash):
+    def update(self,s,a,reward,s_dash,a_dash):
         s = self.digitize(s)
         s_dash = self.digitize(s_dash)
         Qval = self.getQ(s,a)
         Qval_dash = self.getQ(s_dash,a_dash)
         self.Q[a,s[0],s[1]] = Qval + ALPHA * (reward + GAMMA * Qval_dash - Qval)
 
-class LinearFuncAgent:
+class LinearFuncAgent(BasicAgent):
     # 基底関数
     min_list = env.observation_space.low  # 下限値 [-1.2 -0.07]
     max_list = env.observation_space.high  # 上限値 [-0.6, 0.07]
@@ -87,14 +101,14 @@ class LinearFuncAgent:
     norm_factor = norm_factor.reshape(len(norm_factor),1)
 
     def __init__(self,N, sigma, alpha_ini, alpha_last, eps_ini, eps_last):
+        super().__init__(eps_ini,eps_last)
         self.done = False
         self.s1_space = np.linspace(self.min_list[0], self.max_list[0], N)
         self.s2_space = np.linspace(self.min_list[1], self.max_list[1], N)
-        b = (N**NUM_STATE) # 状態空間を分割した場合の総数
         self.alpha_ini = alpha_ini
         self.alpha_last = alpha_last
-        self.eps_ini = eps_ini
-        self.eps_last = eps_last
+
+        b = (N**NUM_STATE) # 状態空間を分割した場合の総数
 
         # 基底関数の定数項を初期化（学習対象外）
         # 基底関数をガウス関数とし、中心値µを初期化
@@ -140,7 +154,7 @@ class LinearFuncAgent:
             return action
 
     # パラメータの更新
-    def train(self, s, a, reward, s_dash, a_dash):
+    def update(self, s, a, reward, s_dash, a_dash):
         X = self.rbfs(s)
         Q_val = self.getQ(s,a)
         Q_val_dash = self.getQ(s_dash,a_dash)
@@ -156,22 +170,20 @@ class LinearFuncAgent:
         Z = np.array([[[agent.getQ(np.array([i,j]),k) for i in x] for j in y] for k in range(NUM_ACTION)])
         return X, Y, Z
 
-
    
-class NNAgent:
+class NNAgent(BasicAgent):
     num_batch = 16
     hidden1 = 32
     hidden2 = 16
-    num_memory = 10000
     min_list = env.observation_space.low  # 下限値 [-1.2 -0.07]
     max_list = env.observation_space.high  # 上限値 [-0.6, 0.07]
 
-    def __init__(self, lr, h1, h2, TAU, num_memory, in_dim, out_dim, eps_ini, eps_last):
+    def __init__(self, lr, h1, h2, in_dim, out_dim, eps_ini, eps_last):
+        super().__init__(eps_ini, eps_last)
         self.done = False
         self.lr = lr    # learning rate
         self.h1 = h1    # 1st hidden layer
         self.h2 = h2    # 2nd hidden layer
-        self.TAU = TAU
         self.in_dim = (in_dim,) # NNモデル生成のために入力変数を指定
         self.out_dim = out_dim  # NNモデル生成のために出力変数を指定 
         self.eps_ini = eps_ini
@@ -209,7 +221,7 @@ class NNAgent:
                 action = np.random.choice(is_greedy_index)
             return action
 
-    def train(self,s,a,reward,s_dash,a_dash):
+    def update(self,s,a,reward,s_dash,a_dash):
         # special reward for DQN
         reward = 0
         if self.done:
@@ -239,9 +251,9 @@ class Result:
 # メイン関数
 
 # 1. Agentの選択
-agent_linear = LinearFuncAgent(N=30, sigma=0.1,alpha_ini=0.2, alpha_last=0, eps_ini=0, eps_last=0)
 agent_table = TableAgent(N=30, alpha_ini=0.5, alpha_last=0, eps_ini=0, eps_last=0)
-agent_NN = NNAgent(lr=1.E-4, h1=320, h2=160, TAU=5, num_memory = 10000, in_dim=NUM_STATE, out_dim=NUM_ACTION, eps_ini=0, eps_last=0)
+agent_linear = LinearFuncAgent(N=30, sigma=0.1,alpha_ini=0.2, alpha_last=0, eps_ini=0, eps_last=0)
+agent_NN = NNAgent(lr=1.E-4, h1=320, h2=160,  in_dim=NUM_STATE, out_dim=NUM_ACTION, eps_ini=0, eps_last=0)
 
 agent = agent_NN # 使用するエージェントを選択
 
@@ -276,7 +288,7 @@ for epi in range(int(num_episode)):
         # s'からe-greedyにより次の行動を決定
         a_dash = agent.select_action(s_dash,EPSILON)
         # 価値（パラメータ）の更新
-        agent.train(s,a,reward,s_dash,a_dash)
+        agent.update(s,a,reward,s_dash,a_dash)
 
         if agent.done:
             if count < 199:
